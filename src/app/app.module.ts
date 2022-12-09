3ff8964a5a3ab6f12da6a1d52cac711b0826b379
azrayildiz <fatma1822yildiz@hotmail.com>
2022-12-09 15:48:21 +0100

    Imported Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './components/header/header.component';
 import { MaterialModule } from './general/material/material.module';
 import { NavigationComponent } from './components/navigation/navigation.component';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { AppRouterModule } from './general/app-router/app-router.module';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { SliderImagesComponent } from './components/slider-images/slider-images.component';
+import { AdvantagesComponent } from './components/advantages/advantages.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    NavigationComponent
+    NavigationComponent,
+    LandingPageComponent,
+    SliderImagesComponent,
+    AdvantagesComponent, 
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule, 
-    MaterialModule
+    MaterialModule, 
+    AppRouterModule, 
+    FontAwesomeModule, 
+
   ],
   providers: [],
   bootstrap: [AppComponent]
